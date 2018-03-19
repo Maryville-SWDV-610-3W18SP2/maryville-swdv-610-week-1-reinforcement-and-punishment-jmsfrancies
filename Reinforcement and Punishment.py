386d5a61b7ff9f8847348bf90a804428979a3ee5
jmsfrancies <35373311+jmsfrancies@users.noreply.github.com>
2018-03-18 20:36:35 -0500

    SWDV 610
    Here is the test

---
+++ Reinforcement and Punishment.py
@@ -0,0 +1,14 @@
+n = int(input("Enter a Number: "))
+m = int(input("Enter a Number to divide the previous number: "))
+def numbers(n,m):
+    z = n % m
+    if z == 0:
+        print("True")
+        return True
+    else:
+        total = 0
+        while total < 100:
+            total = total + 1
+            print("I will never spam my friends again! ")
+        return False
+numbers(n,m)    
